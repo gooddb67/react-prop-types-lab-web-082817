f0a21161f45a33d04473a28d2c01a10f98e9b087
Dave Goodman <davegoodman20@gmail.com>
2017-10-30 18:32:34 -0400

    Done.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,39 @@
-// Code Product Component Here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class Product extends React.Component{
+  render() {
+    return (
+      <ul>
+        <li>{this.props.name}</li>
+        <li>{this.props.producer}</li>
+        <li>{this.props.hasWatermark}</li>
+        <li>{this.props.color}</li>
+        <li>{this.props.hasWatermark}</li>
+        <li>{this.props.weight}</li>
+      </ul>
+    );
+  }
+}
+
+Product.defaultProps = {
+  hasWatermark: false
+};
+
+function rangeCheck(props, propName, componentName){
+  if (!props[propName]){
+    return new Error('no')
+  }else if(typeof props[propName] !== 'number'){
+    return new Error('nooo')
+  }else if(props[propName] < 80 || props[propName] > 300){
+    return new Error('no00000')
+  }
+}
+
+Product.propTypes = {
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.oneOf(["white", "eggshell-white", "salmon"]).isRequired,
+  weight: rangeCheck
+}
